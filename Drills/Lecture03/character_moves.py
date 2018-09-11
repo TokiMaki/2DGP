e6476ae17d59b0a61e0c68ce83281784de174154
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-09-11 18:53:39 +0900

    처음 설정하기
    깃허브를 처음 만듬.

---
+++ Drills/Lecture03/character_moves.py
@@ -0,0 +1,19 @@
+from pico2d import *
+import random 
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 400
+y = 90
+while (1):
+    if (x < 800 & y == 90):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,90)
+    x = x + 2
+    delay(0.01)
+    
+close_canvas()
