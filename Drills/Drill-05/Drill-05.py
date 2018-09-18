733506743db4563c3b22a13e238a771fbcd31d4b
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-09-18 18:47:06 +0900

    Drill-05
    다음 점으로 얼마나 움직여야되는지 구하는 함수를 만듬

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -8,13 +8,19 @@ character = load_image('animation_sheet.png')
 pointx = [203, 132, 535, 477, 715, 316, 510, 692, 682, 712]
 pointy = [535, 234, 470, 203, 136, 225, 92, 518, 336, 349]
 
-def go_next_point():
+def next_direction(num1, num2):
+    x = pointx[num2] - pointx[num1]
+    y = pointy[num2] - pointy[num1]
+    return x, y
+
+def go_next_point(num1, num2):
+    x = pointx[num1]
+    y = pointy[num1]
+    movex, movey = next_direction(num1, num2)
     pass
 
-
 while True:
-    num = 0
-    go_next_point()
+    go_next_point(num, num + 1)
 
 close_canvas()
 
