402f27c71b8960cf8916732b7869aa6cb6a03187
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-09-18 19:10:22 +0900

    Drill-05
    2017182021/양현석
    https://youtu.be/aHuPfh-3Lxs
    커밋횟수 7번

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -5,9 +5,11 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+
 pointx = [203, 132, 535, 477, 715, 316, 510, 692, 682, 712]
 pointy = [535, 234, 470, 203, 136, 225, 92, 518, 336, 349]
 num = 0
+
 
 def next_direction(num1, num2):
     x = pointx[num2] - pointx[num1]
