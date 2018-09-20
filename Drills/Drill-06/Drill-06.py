92927df86eadcc2918d89ccf7256fb5e23483d5e
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-09-20 18:43:00 +0900

    Drill-06
    가는 방향에 따라 달라지는것을 추가함

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -3,10 +3,10 @@ from pico2d import *
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
-
     global running
     global x, y
     global movex, movey
+    global handx, handy
     global dir
     global motion
     global timer
@@ -16,7 +16,9 @@ def handle_events():
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
+            handx, handy = event.x, KPU_HEIGHT - 1 - event.y
             movex, movey = go_point(x, y, event.x, KPU_HEIGHT - 1 - event.y)
+            motion = draw_motion(movex)
             timer = 0
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
@@ -25,6 +27,12 @@ def go_point(characterx, charactery, pointx, pointy):
     movex = (pointx - characterx) // 10.0
     movey = (pointy - charactery) // 10.0
     return movex, movey
+
+def draw_motion(movex):
+    if movex > 0:
+        return 1
+    if movex < 0:
+        return 0
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -39,11 +47,13 @@ movey = 0
 timer = 0
 frame = 0
 motion = 0
+handx, handy = 0, 0
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * motion, 100, 100, x, y)
+    hand_arrow.draw(handx + 25, handy - 25)
     if (timer < 10):
         x += movex
         y += movey
