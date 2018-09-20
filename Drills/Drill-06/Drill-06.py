92c170db8ad000d6041dacea75dbffe40c716dbb
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-09-20 18:38:32 +0900

    Drill-06
    캐릭터가 커서 방향으로 움직이는것 까지 만듬

---
+++ Drills/Drill-06/Drill-06.py
@@ -0,0 +1,58 @@
+from pico2d import *
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+def handle_events():
+    global running
+    global x, y
+    global movex, movey
+    global dir
+    global motion
+    global timer
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            movex, movey = go_point(x, y, event.x, KPU_HEIGHT - 1 - event.y)
+            timer = 0
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+def go_point(characterx, charactery, pointx, pointy):
+    movex = (pointx - characterx) // 10.0
+    movey = (pointy - charactery) // 10.0
+    return movex, movey
+
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+hand_arrow = load_image('hand_arrow.png')
+
+running = True
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+movex = 0
+movey = 0
+timer = 0
+frame = 0
+motion = 0
+
+while running:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * motion, 100, 100, x, y)
+    hand_arrow.draw(handx, handy)
+    if (timer < 10):
+        x += movex
+        y += movey
+        timer += 1
+    frame = (frame + 1) % 8
+    update_canvas()
+
+    handle_events()
+    delay(0.05)
+
+close_canvas()
+
--- Drills/Drill-06/move_character_with_key.py
+++ Drills/Drill-06/move_character_with_key.py
@@ -2,8 +2,37 @@ from pico2d import *
 
 
 def handle_events():
-    # fill here
-    pass
+    global running
+    global x
+    global dir
+    global motion
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+                if motion == 0:
+                    motion = 2
+                else:
+                    motion = 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+                if motion == 1:
+                    motion = 3
+                else:
+                    motion = 0
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+                motion = 3
+            elif event.key == SDLK_LEFT:
+                dir += 1
+                motion = 2
 
 
 open_canvas()
@@ -13,15 +42,18 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
+motion = 0
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * motion, 100, 100, x, 90)
     update_canvas()
 
     handle_events()
     frame = (frame + 1) % 8
+    x += dir * 5
 
     delay(0.05)
 
