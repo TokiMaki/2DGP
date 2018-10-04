acca8ff4d05f5522f5f720c09d866da3bf3bfde4
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-10-04 18:53:26 +0900

    Drill-08-02
    케릭터로 점 위치표시 추가

--- Drills/Drill-08/Drill-08-02.py
+++ Drills/Drill-08/Drill-08-02.py
@@ -14,6 +14,8 @@ def draw_curve_4_points(p1, p2, p3, p4):
     for i in range(0, 100, 2):
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH / 2, KPU_HEIGHT / 2)
+        for j in range(0, 10, 1):
+            character.clip_draw(0, 0, 100, 100, points[j][0], points[j][1])
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (-3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (-3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
