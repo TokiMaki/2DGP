ede4f8338f119c62e7fd7e5a0582fea2687b154a
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-10-04 19:15:19 +0900

    Drill-08-02
    2017182021 양현석 커밋횟수:7회https://youtu.be/9HgTKMcsyG0

--- Drills/Drill-08/Drill-08-02.py
+++ Drills/Drill-08/Drill-08-02.py
@@ -7,16 +7,18 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+j = 1
 
 def draw_curve_4_points(p1, p2, p3, p4):
     frame = 0
     motion = 2
+    global j
     clear_canvas()
     for i in range(0, 100, 2):
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH / 2, KPU_HEIGHT / 2)
-        for j in range(0, 10, 1):
-            character.clip_draw(0, 0, 100, 100, points[j][0], points[j][1])
+        for k in range(0, j, 1):
+            character.clip_draw(0, 0, 100, 100, points[k][0], points[k][1])
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (-3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (-3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
@@ -37,5 +39,7 @@ n = 0
 while True:
     draw_curve_4_points(points[(n) % 10], points[(n+1) % 10], points[(n+2) % 10], points[(n+3) % 10])
     n = (n + 1) % size
+    if (j < 10):
+        j = j + 1
 
 
