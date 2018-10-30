7249793433de3e15c60531d3185ccddc716b3464
TokiMaki <40135375+TokiMaki@users.noreply.github.com>
2018-10-30 21:22:24 +0900

    Update Drills/Drill-12/ghost.py
    Update 만듬

--- Drills/Drill-12/ghost.py
+++ Drills/Drill-12/ghost.py
@@ -25,3 +25,26 @@ class Ghost:
     def draw(self):
         self.image.opacify(random.randint(1, 10) / 10.0)
         self.image.clip_composite_draw(int(self.frame) * 100, 300, 100, 100, 3.141592 / 2, '', self.x_cos, self.y_sin, 100, 100)
+
+    def update(self):
+        self.frame = (self.frame + boy.FRAMES_PER_ACTION * boy.ACTION_PER_TIME * game_framework.frame_time) % 8
+        if (self.state == 0):
+            self.r += boy.RUN_SPEED_PPS * game_framework.frame_time
+            self.x_cos = self.x + math.cos(math.pi / 180 * self.rotate) * self.r
+            self.y_sin = self.y + math.sin(math.pi / 180 * self.rotate) * self.r
+            if (self.r >= 100):
+                self.r = 100
+                self.state = 1
+
+        if (self.state == 1):
+            boy.real_time += game_framework.frame_time
+            self.rotate += 720 * game_framework.frame_time
+            self.x_cos = self.x + math.cos(math.pi / 180 * self.rotate) * self.r
+            self.y_sin = self.y + math.sin(math.pi / 180 * self.rotate) * self.r
+
+        if (boy.real_time >= 1.0):
+            boy.real_time = boy.real_time % 1
+
+        if (main_state.boy.cur_state != boy.SleepState):
+            game_world.remove_object(self)
+
